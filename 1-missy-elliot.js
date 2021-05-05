5e10667f879684ff035f8d8ad5d35157eb036afd
Rome Bell <rome.bell@generalassemb.ly>
2021-05-05 09:32:43 -0700

    Create Missy Elliot problem

---
+++ 1-missy-elliot.js
@@ -0,0 +1,17 @@
+/***********************************************************************
+Write a function missyElliot(array) that takes in an array of strings and returns a sentence string that flip each string element in the array and reverses the entire array. 
+
+Examples:
+const arr = [ 'siht', 'esrever', 'dna', 'pilf', 'attog', 'uoY' ];
+const lyricsArr = ['ti', 'esrever', 'dna', 'ti', 'pilf', 'nwod', 'gnaht', 'ym', 'tup', 'I'];
+
+missyElliot(arr); // You gotta flip and reverse this
+missyElliot(lyricsArr); // I put my thang down flip it and reverse it
+***********************************************************************/
+
+function missyElliot(array) {
+
+}
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+module.exports = oddWordsOut;
