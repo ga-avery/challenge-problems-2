66d2266172accf97be3e0db15c595fe19263252f
Rome Bell <rome.bell@generalassemb.ly>
2021-05-05 09:41:28 -0700

    Create ps and qs

---
+++ 3-mind-ps-and-qs.js
@@ -0,0 +1,25 @@
+/***********************************************************************
+Write a function mindPsAndQs(str) that accepts a string of uppercase
+letters. The function should return the length of the longest consecutive
+streak of the letters 'P' and 'Q'.
+
+Hint: Use two variables. One to track the length of the current streak
+and another to track the length of the longest streak so far. This can also be solved using a
+single loop!
+
+Nested loops not needed!
+
+Examples:
+mindPsAndQs('ENGINEERING'); // => 0
+mindPsAndQs('APCDQQPPC'); // => 4
+mindPsAndQs('PQPQ'); // => 4
+mindPsAndQs('PPPXQPPPQ'); // => 5
+***********************************************************************/
+
+
+function mindPsAndQs(str) {
+
+}
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+module.exports = mindPsAndQs;
